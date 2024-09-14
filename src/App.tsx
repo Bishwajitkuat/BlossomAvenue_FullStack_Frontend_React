876cec12cf0e7c8cc7b840bbcb286b6081d2673b
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-14 15:20:00 +0300

    Install react-query and react-query-devtools Configure and Proive these to all components

--- src/App.tsx
+++ src/App.tsx
@@ -8,60 +8,73 @@ import ProtectedRoutes from "./features/Authorizations/ProtectedRoutes";
 import Cart from "./pages/Cart";
 import EmployeeAdminProtectedRoutes from "./features/Authorizations/EmployeeAdminProtectedRoutes";
 import AdminProtectedRoutes from "./features/Authorizations/AdminProtectedRoutes";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="products" element={<Products />} />
-          <Route path="products/:productId" element={<Product />} />
-          <Route path="user" element={<ProtectedRoutes />}>
-            <Route
-              path=""
-              element={<h1>User profile: not implemented yet</h1>}
-            />
-            <Route path="cart" element={<Cart />} />
-            <Route path="orders" element={<h1>not implemented yet</h1>} />
-            <Route
-              path="orders/:orderId"
-              element={<h1>not implemented yet</h1>}
-            />
-            <Route
-              path="orders/create"
-              element={<h1>not implemented yet</h1>}
-            />
-            <Route path="employee" element={<EmployeeAdminProtectedRoutes />}>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="products" element={<Products />} />
+            <Route path="products/:productId" element={<Product />} />
+            <Route path="user" element={<ProtectedRoutes />}>
               <Route
                 path=""
-                element={<h1>Employee dashboard not implemented yet</h1>}
+                element={<h1>User profile: not implemented yet</h1>}
               />
+              <Route path="cart" element={<Cart />} />
+              <Route path="orders" element={<h1>not implemented yet</h1>} />
               <Route
-                path="products/create"
+                path="orders/:orderId"
                 element={<h1>not implemented yet</h1>}
               />
               <Route
-                path="products/update/:productId"
+                path="orders/create"
                 element={<h1>not implemented yet</h1>}
               />
-            </Route>
-            <Route path="admin" element={<AdminProtectedRoutes />}>
-              <Route
-                path=""
-                element={<h1>Admin dashboard not implemented yet</h1>}
-              />
-              <Route path="users" element={<h1>not implemented yet</h1>} />
-              <Route
-                path="users/update/:userId"
-                element={<h1>not implemented yet</h1>}
-              />
+              <Route path="employee" element={<EmployeeAdminProtectedRoutes />}>
+                <Route
+                  path=""
+                  element={<h1>Employee dashboard not implemented yet</h1>}
+                />
+                <Route
+                  path="products/create"
+                  element={<h1>not implemented yet</h1>}
+                />
+                <Route
+                  path="products/update/:productId"
+                  element={<h1>not implemented yet</h1>}
+                />
+              </Route>
+              <Route path="admin" element={<AdminProtectedRoutes />}>
+                <Route
+                  path=""
+                  element={<h1>Admin dashboard not implemented yet</h1>}
+                />
+                <Route path="users" element={<h1>not implemented yet</h1>} />
+                <Route
+                  path="users/update/:userId"
+                  element={<h1>not implemented yet</h1>}
+                />
+              </Route>
             </Route>
           </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
