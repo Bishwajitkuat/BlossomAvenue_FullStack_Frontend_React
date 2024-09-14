2e81cb825ee5d4c04caf5ce4a834b0dbfa25962e
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-14 13:35:17 +0300

    Add project structure and basic routings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./layout/AppLayout";
+import { Home } from "./pages/Home";
+import Login from "./pages/Login";
+import Products from "./pages/Products";
+import Product from "./pages/Product";
+import ProtectedRoutes from "./features/Authorizations/ProtectedRoutes";
+import Cart from "./pages/Cart";
+import EmployeeAdminProtectedRoutes from "./features/Authorizations/EmployeeAdminProtectedRoutes";
+import AdminProtectedRoutes from "./features/Authorizations/AdminProtectedRoutes";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:productId" element={<Product />} />
+          <Route path="user" element={<ProtectedRoutes />}>
+            <Route
+              path=""
+              element={<h1>User profile: not implemented yet</h1>}
+            />
+            <Route path="cart" element={<Cart />} />
+            <Route path="orders" element={<h1>not implemented yet</h1>} />
+            <Route
+              path="orders/:orderId"
+              element={<h1>not implemented yet</h1>}
+            />
+            <Route
+              path="orders/create"
+              element={<h1>not implemented yet</h1>}
+            />
+            <Route path="employee" element={<EmployeeAdminProtectedRoutes />}>
+              <Route
+                path=""
+                element={<h1>Employee dashboard not implemented yet</h1>}
+              />
+              <Route
+                path="products/create"
+                element={<h1>not implemented yet</h1>}
+              />
+              <Route
+                path="products/update/:productId"
+                element={<h1>not implemented yet</h1>}
+              />
+            </Route>
+            <Route path="admin" element={<AdminProtectedRoutes />}>
+              <Route
+                path=""
+                element={<h1>Admin dashboard not implemented yet</h1>}
+              />
+              <Route path="users" element={<h1>not implemented yet</h1>} />
+              <Route
+                path="users/update/:userId"
+                element={<h1>not implemented yet</h1>}
+              />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/features/Authorizations/EmployeeAdminProtectedRoutes.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const EmployeeAdminProtectedRoutes = () => {
+  return <Outlet />;
+};
+
+export default EmployeeAdminProtectedRoutes;
