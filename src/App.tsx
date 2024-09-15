a503fdc7f3b9fdd4e595a47fd8c1768c4af0533b
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-15 14:43:15 +0300

    Add Toaster component and configure it

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import AdminProtectedRoutes from "./features/Authorizations/AdminProtectedRoutes
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Profile from "./pages/Profile";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -72,6 +73,26 @@ const App = () => {
           </Route>
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-right"
+        gutter={12}
+        containerStyle={{ margin: "0.5rem" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "1.5rem",
+            maxWidth: "40rem",
+            padding: "1.5rem, 2.5rem",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 };
