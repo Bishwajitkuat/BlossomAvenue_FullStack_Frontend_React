ffab167a8c876f684f7c1815d9656ac201566a78
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-15 03:14:25 +0300

    Add a private instance of axios

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -6,3 +6,8 @@ export const axiosPublic = axios.create({
   baseURL: BASE_URL,
 });
 
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
