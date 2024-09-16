4eb3fc8b923ba2e91e1ea3e7cc3227e4a64c0137
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-16 13:18:54 +0300

    Fetch products from backend via useGetProducts custom hook provide error notification or render Loader or AllProducts component depending on states.

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AllProducts from "../features/Products/AllProducts";
+import useGetProducts from "../hooks/products/useGetProducts";
+import toast from "react-hot-toast";
+import Loader from "../components/ui/Loader";
 
 const Products = () => {
-  return <div>Products</div>;
+  const { isProductsLoading, isError, error, paginatedProducts } =
+    useGetProducts();
+
+  useEffect(() => {
+    if (isError && error) toast.error(error.message);
+  }, [isError]);
+  if (isProductsLoading) return <Loader />;
+  return (
+    <div>
+      <AllProducts products={paginatedProducts?.items} />
+    </div>
+  );
 };
 
 export default Products;
