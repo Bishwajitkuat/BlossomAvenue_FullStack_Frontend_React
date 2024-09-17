8339fc89f6d819a82988241f99ac275bd04f8c57
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-17 04:34:15 +0300

    Add max width so that components does not over stretch on wider screens.

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -7,7 +7,9 @@ const AppLayout = () => {
     <div className="flex h-screen w-screen min-w-[360px] flex-col">
       <Header />
       <main className="flex-1 grow overflow-y-auto bg-gradient-to-br from-zinc-400 to-zinc-900">
-        <Outlet />
+        <div className="max-w-[1280px] mx-auto">
+          <Outlet />
+        </div>
       </main>
       <Footer />
     </div>
