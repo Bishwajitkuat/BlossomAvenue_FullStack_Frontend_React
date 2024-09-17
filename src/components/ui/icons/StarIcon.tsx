ed0e628d78f99c9489fe7a32fb05811f786c9e94
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-17 22:09:25 +0300

    Add StarIcon component

---
+++ src/components/ui/icons/StarIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function StarIcon() {
+  return (
+    <svg
+      fill="none"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 53.867 53.867"
+      className="h-9 w-9 fill-orange-600 stroke-orange-600"
+    >
+      <polygon
+        // style="fill:#EFCE4A;"
+        points="26.934,1.318 35.256,18.182 53.867,20.887 40.4,34.013 43.579,52.549 26.934,43.798 
+	10.288,52.549 13.467,34.013 0,20.887 18.611,18.182 "
+      />
+    </svg>
+  );
+}
+
+export default StarIcon;
