8f32979801b86e25127009ff64940d08eda007d0
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-17 04:23:23 +0300

    Add types related to Get product by id.

--- src/utils/types/product.ts
+++ src/utils/types/product.ts
@@ -15,3 +15,48 @@ export interface GetAllProductReadDto {
   inventory: number;
   avgStar: number;
 }
+
+export interface ReadProductReviewDto {
+  reviewId: string;
+  review: string | null;
+  star: number | null;
+  userName: string;
+}
+
+export interface ReadProductCategoryDto {
+  productCategoryId: string;
+  categoryId: string;
+  productId: string;
+  categoryName: string;
+}
+export interface Image {
+  imageId: string;
+  imageUrl: string;
+  productId: string;
+}
+
+export interface Variation {
+  variationId: string;
+  variationName: string;
+  price: number;
+  inventory: number;
+  productId: string;
+}
+
+export interface GetProductByIdReadDto {
+  productId: string;
+  title: string;
+  description: string;
+  images: Image[];
+  variations: Variation[];
+  productCategories: ReadProductCategoryDto[];
+  productReviews: ReadProductReviewDto[];
+  avgStar: number;
+}
+
+export interface ProductReviewCreateDto {
+  userId: string | null;
+  productId: string | null;
+  review: string;
+  star: string;
+}
