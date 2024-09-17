fb08f36664db0e008730a32d1f098eaa6b1f31ee
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-17 22:08:59 +0300

    Update the query string related to page no and size when data came back from backend.

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 import { PaginationPropType } from "../utils/types/pagination";
+import { useEffect } from "react";
 
 function Pagination({
   currentPage,
@@ -28,9 +29,14 @@ function Pagination({
     searchParams.set("PageSize", count);
     setSearchParams(searchParams);
   };
+  useEffect(() => {
+    searchParams.set("PageSize", itemPerPage ? itemPerPage.toString() : "10");
+    searchParams.set("PageNo", currentPage ? currentPage.toString() : "1");
+    setSearchParams(searchParams);
+  }, [itemPerPage, currentPage]);
   return (
-    <div className="w-full flex gap-8 justify-center min-h-[4rem]">
-      <div className="grid grid-cols-3 gap-4">
+    <div className="w-full mt-[5rem] pb-[5rem] flex gap-8 justify-center items-center min-h-[4rem]">
+      <div className="grid grid-cols-3 gap-4 items-center">
         <button
           onClick={previousPage}
           disabled={currentPage === 1}
@@ -39,6 +45,7 @@ function Pagination({
               ? "You are in the first page"
               : "go to previous page"
           }
+          className="w-full  rounded-b-md bg-pink-300 px-4 py-3  uppercase tracking-widest shadow-md shadow-zinc-500 outline-none duration-200 ease-in hover:bg-pink-500"
         >
           Previous
         </button>
@@ -53,11 +60,12 @@ function Pagination({
               ? "You are in the last page"
               : "go to next page."
           }
+          className="w-full  rounded-b-md bg-pink-300 px-4 py-3 uppercase tracking-widest shadow-md shadow-zinc-500 outline-none duration-200 ease-in hover:bg-pink-500"
         >
           Next
         </button>
       </div>
-      <div className="flex gap-4 items-center">
+      <div className="grid grid-cols-2 gap-4 items-center">
         <p>Item per page</p>
         <select
           className="bg-pink-200 outline-none"
