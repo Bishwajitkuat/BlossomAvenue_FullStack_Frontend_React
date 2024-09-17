e6fba8ef264a6b59fc422c93a0ae3b37d78bde63
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-17 15:20:11 +0300

    Add PaginationPropType type

---
+++ src/utils/types/pagination.ts
@@ -0,0 +1,6 @@
+export interface PaginationPropType {
+  currentPage: number | undefined;
+  itemPerPage: number | undefined;
+  totalItemCount: number | undefined;
+  totalPageCount: number | undefined;
+}
