d8f7002ac0270f8482a68b3d38e238fda0da40be
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-17 15:23:51 +0300

    Update useGetProducts custom hook. Read query params and create updated query string to fetch products.

--- src/hooks/products/useGetProducts.tsx
+++ src/hooks/products/useGetProducts.tsx
@@ -4,13 +4,22 @@ import {
   GetAllProductReadDto,
   PaginatedResponse,
 } from "../../utils/types/product";
+import { useSearchParams } from "react-router-dom";
 
 function useGetProducts() {
   // add pagination and filtering
+  const [searchParams] = useSearchParams();
+  const pageSize = searchParams.get("PageSize")
+    ? searchParams.get("PageSize")
+    : "10";
+  const pageNo = searchParams.get("PageNo") ? searchParams.get("PageNo") : "1";
+
+  const routeString = `/products?PageSize=${pageSize}&PageNo=${pageNo}`;
+
   const getProducts = async (): Promise<
     PaginatedResponse<GetAllProductReadDto>
   > => {
-    const res = await axiosPublic.get("/products");
+    const res = await axiosPublic.get(routeString);
     return res.data;
   };
 
