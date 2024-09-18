72c868c295e6887e11eaf72d8d21c38640ac2aab
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-18 20:52:36 +0300

    Add useCreateProduct custom hook to create new product.

---
+++ src/hooks/products/useCreateProduct.tsx
@@ -0,0 +1,32 @@
+import {
+  CreateProductDto,
+  GetProductByIdReadDto,
+} from "../../utils/types/product";
+import useAxiosPrivate from "../useAxiosPrivate";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+function useCreateProduct() {
+  const axiosPrivate = useAxiosPrivate();
+  const navigation = useNavigate();
+  const addProduct = async (
+    product: CreateProductDto
+  ): Promise<GetProductByIdReadDto> => {
+    const res = await axiosPrivate.post("/products", product);
+    return res.data;
+  };
+
+  const { isPending: isCreateProductLoading, mutate: createProduct } =
+    useMutation({
+      mutationFn: addProduct,
+      onSuccess: (data) => {
+        toast.success("Product is added successfully!");
+        navigation(`/products/${data.productId}`);
+      },
+      onError: (err) => toast(err.message),
+    });
+  return { isCreateProductLoading, createProduct };
+}
+
+export default useCreateProduct;
