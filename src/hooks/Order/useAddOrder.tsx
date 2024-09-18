a8e1ac25c6d4c1431e7a53a62f5baab6737a124f
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-18 14:50:52 +0300

    Bug fix: not updating cart data after making an order.

--- src/hooks/Order/useAddOrder.tsx
+++ src/hooks/Order/useAddOrder.tsx
@@ -1,12 +1,13 @@
 import toast from "react-hot-toast";
 import { CreateOrderDto, ReadOrderDto } from "../../utils/types/order";
 import useAxiosPrivate from "../useAxiosPrivate";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 
 function useAddOrder() {
   const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const addOrder = async (order: CreateOrderDto): Promise<ReadOrderDto> => {
     const res = await axiosPrivate.post("/orders", order);
     return res.data;
@@ -15,6 +16,7 @@ function useAddOrder() {
   const { isPending: isAddOrderLoading, mutate: addNewOrder } = useMutation({
     mutationFn: addOrder,
     onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["cart"] });
       toast.success("The order is placed successfully!");
       navigate(`/user/orders/${data.orderId}`);
     },
